ce44d766a927c2a0cfad6e0d627c2638f824e0b9
Samuel Witte <witte.sam@gmail.com>
2026-07-09 10:38:17 +0200

    fix full eql false

--- src/solve_sr_rates.jl
+++ src/solve_sr_rates.jl
@@ -1034,7 +1034,7 @@ function find_im_part(mu, M, a, n, l, m; debug=false, Ntot_force=5000, iter=5000
                 if SR211_g == 0
                     SR211_g = 1e-10 .* mu
                 end
-                w0 = (ergL(n, l, m, alph ./ (GNew * M), M, a; full=false) .+ im * SR211_g) .* GNew * M
+                w0 = (ergL(n, l, m, alph ./ (GNew * M), M, a; full=true) .+ im * SR211_g) .* GNew * M
                 # print("test \t ", w0, "\n")
             else
                 w0 = erg_Guess
